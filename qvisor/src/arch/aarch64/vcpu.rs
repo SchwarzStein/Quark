ef37da1f9847dede8aa460f0c4b30759e8b6b158
ch1337 <christo.bita@mailbox.tu-dresden.de>
2024-11-04 00:32:38 +0100

    vcpu refactor: VirtCpu trait: extend API; provide defaults

--- qvisor/src/arch/aarch64/vcpu.rs
+++ qvisor/src/arch/aarch64/vcpu.rs
@@ -137,7 +137,7 @@ impl VirtCpu for Aarch64VirtCpu {
         self._run()
     }
 
-    fn default_hypercall_handler(&self, hypercall: u16, _data: &[u8], arg0: u64, arg1: u64,
+    fn default_hypercall_handler(&self, hypercall: u16, arg0: u64, arg1: u64,
         arg2: u64, arg3: u64) -> Result<bool, Error> {
         let id = self.vcpu_base.id;
         match hypercall {
@@ -398,11 +398,11 @@ impl Aarch64VirtCpu {
                 let (arg0, arg1, arg2, arg3) = self.conf_comp_extension
                     .get_hypercall_arguments(&self.vcpu_base.vcpu_fd, self.vcpu_base.id)?;
                 if self.conf_comp_extension.should_handle_hypercall(hypercall) {
-                    exit_loop = self.conf_comp_extension.handle_hypercall(hypercall, data, arg0,
+                    exit_loop = self.conf_comp_extension.handle_hypercall(hypercall, arg0,
                         arg1, arg2, arg3, self.vcpu_base.id)
                         .expect("VM run failed - cannot handle hypercall correctly.");
                 } else {
-                    exit_loop = self.default_hypercall_handler(hypercall, data, arg0, arg1,
+                    exit_loop = self.default_hypercall_handler(hypercall, arg0, arg1,
                         arg2, arg3)
                         .expect("VM run failed - cannot handle hypercall correctly.");
                 }
