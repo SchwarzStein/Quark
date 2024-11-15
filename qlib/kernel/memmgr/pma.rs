yaoxin-jing/cc_quark_christo

cc: pagetable: adjust pagetable functions:
        * handle when host prepares PT for guest
        * handle IPA shared bit for TEE

qlib/kernel/memmgr/pma.rs
@@ -32,6 +32,7 @@ use super::super::PAGE_MGR;
 
 use crate::kernel_def::Invlpg;
 //use crate::qlib::kernel::memmgr::pmamgr::PagePool;
+use crate::qlib::kernel::arch::tee;
 
 pub type PageMgrRef = ObjectRef<PageMgr>;
 
@@ -236,20 +237,22 @@ impl PageTables {
             if pgdEntry.is_unused() {
                 return Err(Error::AddressNotMap(0));
             }
-            let pudTbl = pgdEntry.addr().as_u64() as *const PageTable;
+            let pudTbl = tee::guest_physical_address(pgdEntry.addr().as_u64()) as *const PageTable;
             let nPt: *mut PageTable = ret.GetRoot() as *mut PageTable;
             let nPgdEntry = &mut (*nPt)[0];
             let nPudTbl = pagePool.AllocPage(true)? as *mut PageTable;
+            let mut table = nPudTbl as u64;
+            tee::gpa_to_ipa(&mut table, true);
             #[cfg(target_arch = "x86_64")]
             nPgdEntry.set_addr(
-                PhysAddr::new(nPudTbl as u64),
+                PhysAddr::new(table),
                 PageTableFlags::PRESENT
                     | PageTableFlags::WRITABLE
                     | PageTableFlags::USER_ACCESSIBLE,
             );
             #[cfg(target_arch = "aarch64")]
             nPgdEntry.set_addr(
-                PhysAddr::new(nPudTbl as u64),
+                PhysAddr::new(table),
                 PageTableFlags::VALID
                     | PageTableFlags::TABLE
                     | PageTableFlags::ACCESSED

SchwarzStein/Quark

qlib/kernel/memmgr/pma.rs
@@ -32,6 +32,7 @@ use super::super::PAGE_MGR;
 
 use crate::kernel_def::Invlpg;
 //use crate::qlib::kernel::memmgr::pmamgr::PagePool;
+use crate::qlib::kernel::arch::tee;
 
 pub type PageMgrRef = ObjectRef<PageMgr>;
 
@@ -236,20 +237,22 @@ impl PageTables {
             if pgdEntry.is_unused() {
                 return Err(Error::AddressNotMap(0));
             }
-            let pudTbl = pgdEntry.addr().as_u64() as *const PageTable;
+            let pudTbl = tee::guest_physical_address(pgdEntry.addr().as_u64()) as *const PageTable;
             let nPt: *mut PageTable = ret.GetRoot() as *mut PageTable;
             let nPgdEntry = &mut (*nPt)[0];
             let nPudTbl = pagePool.AllocPage(true)? as *mut PageTable;
+            let mut table = nPudTbl as u64;
+            tee::gpa_adjust_shared_bit(&mut table, true);
             #[cfg(target_arch = "x86_64")]
             nPgdEntry.set_addr(
-                PhysAddr::new(nPudTbl as u64),
+                PhysAddr::new(table),
                 PageTableFlags::PRESENT
                     | PageTableFlags::WRITABLE
                     | PageTableFlags::USER_ACCESSIBLE,
             );
             #[cfg(target_arch = "aarch64")]
             nPgdEntry.set_addr(
-                PhysAddr::new(nPudTbl as u64),
+                PhysAddr::new(table),
                 PageTableFlags::VALID
                     | PageTableFlags::TABLE
                     | PageTableFlags::ACCESSED