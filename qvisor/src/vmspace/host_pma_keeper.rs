yaoxin-jing/cc_quark_christo

cc: aarch64: CCA: use read/write syscall to map files
        * mmap seems to break Realm, so we need another protocol
        * dedicated qcall to write back changes in the buffered file

qvisor/src/vmspace/host_pma_keeper.rs
@@ -13,8 +13,14 @@
 // limitations under the License.
 
 use alloc::collections::BTreeSet;
+use hashbrown::HashMap;
 use spin::Mutex;
 use std::collections::VecDeque;
+use std::io::Read;
+use std::os::fd::FromRawFd;
+use std::os::unix::fs::FileExt;
+
+use crate::qlib::kernel::arch::tee::is_hw_tee;
 
 use super::super::heap_alloc::ENABLE_HUGEPAGE;
 use super::super::memmgr::*;
@@ -36,12 +42,15 @@ impl AreaValue for HostSegment {
     }
 }
 
-//pub struct HostPMAKeeper (Mutex<AreaSet<HostSegment>>);
-
 pub struct HostPMAKeeper {
     pub ranges: Mutex<AreaSet<HostSegment>>,
     pub hugePages: Mutex<VecDeque<u64>>,
     pub allocPages: Mutex<BTreeSet<u64>>,
+    //NOTE: We need this only for Realm
+    //  We could make this an Option, but will
+    //  require to put PMA_KEERP befind a mutex
+    #[cfg(target_arch = "aarch64")]
+    file_on_range: Mutex<HashMap<(u64, u64), (std::fs::File, u64)>>,
 }
 
 impl HostPMAKeeper {
@@ -50,6 +59,8 @@ impl HostPMAKeeper {
             ranges: Mutex::new(AreaSet::New(0, 0)),
             hugePages: Mutex::new(VecDeque::with_capacity(1000)),
             allocPages: Mutex::new(BTreeSet::new()),
+            #[cfg(target_arch = "aarch64")]
+            file_on_range: Mutex::new(HashMap::new()),
         };
     }
 
@@ -99,7 +110,7 @@ impl HostPMAKeeper {
 
     pub fn InitHugePages(&self) {
         let hugeLen = (self.ranges.lock().range.Len() / MemoryDef::ONE_GB - 2) * MemoryDef::ONE_GB;
-        //error!("InitHugePages is {:x}", self.ranges.lock().range.Len() / MemoryDef::ONE_GB - 2);
+        error!("InitHugePages - Len is {:x}G", self.ranges.lock().range.Len() / MemoryDef::ONE_GB - 2);
         let hugePageStart = self
             .RangeAllocate(hugeLen, MemoryDef::PAGE_SIZE_2M)
             .unwrap();
@@ -153,26 +164,54 @@ impl HostPMAKeeper {
         return self.Map(&mut mo, &Range::New(start, len));
     }
 
-    pub fn MapFile(&self, len: u64, prot: i32, fd: i32, offset: u64) -> Result<u64> {
-        let osfd = fd;
+    fn __map_file_directly(&self, mapping_range: Range, prot: i32, fd: i32, offset: u64)
+        -> Result<u64> {
         let mut mo = &mut MapOption::New();
-
         mo = mo
             .Proto(prot)
             .FileOffset(offset)
-            .FileId(osfd)
-            .Len(len)
-            .MapFixed();
+            .FileId(fd)
+            .Len(mapping_range.Len())
+            .Addr(mapping_range.Start())
+            .MapFixed()
+            .MapShare();
 
-        mo.MapShare();
-        // mo.MapPrecommit();
+        let ret = self.Map(&mut mo, &mapping_range)?;
+        Ok(ret)
+    }
 
-        //mo.MapLocked();
+    fn __map_file_buffered(&self, mapping_range: Range, _prot: i32, fd: i32, offset: u64)
+        -> Result<u64> {
+        let mut file = unsafe { std::fs::File::from_raw_fd(fd) };
+        let mut rf_slice = unsafe {
+            std::slice::from_raw_parts_mut(mapping_range.Start() as *mut u8,
+                mapping_range.Len() as usize)
+        };
+        let actual_read = file.read(&mut rf_slice);
+        if actual_read.is_ok() {
+            debug!("VMM: Map file - Read from file: bytes{:#0x}", actual_read.unwrap());
+        } else {
+            debug!("VMM: Map File - read failed.");
+        }
+        debug!("VMM: Map file with FD:{}, len:{:#0x}B, host mapped:{:#0x}",
+            fd, mapping_range.Len(), mapping_range.Start());
+        self.file_on_range.lock().insert((mapping_range.Start(), mapping_range.Len()),
+            (file, offset));
+        Ok(mapping_range.Start())
+    }
 
-        let start = self.Allocate(len, MemoryDef::PMD_SIZE)?;
-        mo.Addr(start);
-        let ret = self.Map(&mut mo, &Range::New(start, len));
-        return ret;
+    pub fn MapFile(&self, length: u64, prot: i32, fd: i32, offset: u64) -> Result<u64> {
+        debug!("VMM: Map file with FD:{}, len:{:#0x} Bytes, offset:{:#0x}, PROT_flags:{:#0x}",
+            fd, length, offset, prot);
+        let start_addr = self.Allocate(length, MemoryDef::PMD_SIZE)?;
+        let mapping_range = Range {start: start_addr, len: length};
+        #[cfg(target_arch = "aarch64")]
+        if is_hw_tee() {
+            let r = self.__map_file_buffered(mapping_range, prot, fd, offset);
+            return r;
+        }
+
+        self.__map_file_directly(mapping_range, prot, fd, offset)
     }
 
     fn RangeAllocate(&self, len: u64, alignment: u64) -> Result<u64> {
@@ -224,15 +263,72 @@ impl HostPMAKeeper {
     }
 
     pub fn Unmap(&self, r: &Range) -> Result<()> {
-        //self.RemoveSeg(r);
-
         assert!(r.Start() % MemoryDef::PAGE_SIZE_2M == 0);
         assert!(r.Len() == MemoryDef::PAGE_SIZE_2M);
 
         self.FreeHugePage(r.Start());
 
+        debug!("VMM: Unmap file len:{:#0x}B, guest mapped:{:#0x}", r.Len(), r.Start());
+        #[cfg(target_arch = "aarch64")] {
+            if is_hw_tee() {
+                let f = self.file_on_range.lock().remove(&(r.Start(), r.Len()));
+                if f.is_some() {
+                    debug!("VMM: unmap file from:{:#0x} - length:{:#0x}.", r.Start(), r.Len());
+                } else {
+                    debug!("VMM: no file found at:{:#0x}", r.Start());
+                }
+            }
+        }
+
         let res = MapOption::MUnmap(r.Start(), r.Len());
         return res;
+    }
+
+    #[cfg(target_arch = "aarch64")]
+    pub fn write_back(&self, data_heap_address: u64, data_size: usize) -> u64 {
+        let ranges_array = unsafe {
+            std::slice::from_raw_parts(data_heap_address as *const Range, data_size)
+        };
+        debug!("VMM: Write back changes to the file");
+        let range_start = ranges_array[0].Start();
+        let rs_size = ranges_array[0].Len();
+        for (_key, _file) in self.file_on_range.lock().iter() {
+            if _key.0 <= range_start && (_key.0 + _key.1 > range_start) {
+                debug!("VMM: Found file buffered at:{:#0x} - length:{:#0x}", range_start, rs_size);
+                let  file_offset = _file.1;
+                let mut write_back_range: Range =
+                    Range { start: range_start, len: rs_size };
+                for i in 1..data_size {
+                    if write_back_range.Start() + write_back_range.Len() + 1
+                        == ranges_array[i].Start() {
+                        write_back_range.len += ranges_array[i].Len();
+                    } else {
+                        Self::__write_back(&_file.0, &write_back_range, file_offset, _key.0);
+                        write_back_range.start = ranges_array[i].Start();
+                        write_back_range.len = ranges_array[i].Len();
+                    }
+                }
+                Self::__write_back(&_file.0, &write_back_range, file_offset, _key.0);
+                break;
+            }
+        }
+        0
+    }
+
+    #[cfg(target_arch = "aarch64")]
+    fn __write_back(file: &std::fs::File, range: &Range, file_offset: u64, mapping_start: u64) {
+        let buff = unsafe {
+            std::slice::from_raw_parts(range.Start() as *const u8, range.Len() as usize)
+        };
+        let _offset = file_offset + (range.Start() - mapping_start);
+        let written = file.write_at(buff, _offset);
+        if written.is_ok() {
+            debug!("VMM: write back to file offset:{:#0x} - at:{:#0x}\
+                - len:{:#0x} B - written:{:#0x} B", _offset, range.Start(),
+                range.Len(), written.unwrap());
+        } else {
+            panic!("VMM: write back failed - could not write to file.");
+        }
     }
 }
 

SchwarzStein/Quark

qvisor/src/vmspace/host_pma_keeper.rs
@@ -13,8 +13,18 @@
 // limitations under the License.
 
 use alloc::collections::BTreeSet;
+#[cfg(target_arch = "aarch64")]
+use hashbrown::HashMap;
 use spin::Mutex;
 use std::collections::VecDeque;
+#[cfg(target_arch = "aarch64")]
+use std::io::Read;
+#[cfg(target_arch = "aarch64")]
+use std::os::fd::FromRawFd;
+#[cfg(target_arch = "aarch64")]
+use std::os::unix::fs::FileExt;
+#[cfg(target_arch = "aarch64")]
+use crate::qlib::kernel::arch::tee::is_hw_tee;
 
 use super::super::heap_alloc::ENABLE_HUGEPAGE;
 use super::super::memmgr::*;
@@ -36,12 +46,15 @@ impl AreaValue for HostSegment {
     }
 }
 
-//pub struct HostPMAKeeper (Mutex<AreaSet<HostSegment>>);
-
 pub struct HostPMAKeeper {
     pub ranges: Mutex<AreaSet<HostSegment>>,
     pub hugePages: Mutex<VecDeque<u64>>,
     pub allocPages: Mutex<BTreeSet<u64>>,
+    //NOTE: We need this only for Realm
+    //  We could make this an Option, but will
+    //  require to put PMA_KEERP befind a mutex
+    #[cfg(target_arch = "aarch64")]
+    file_on_range: Mutex<HashMap<(u64, u64), (std::fs::File, u64)>>,
 }
 
 impl HostPMAKeeper {
@@ -50,6 +63,8 @@ impl HostPMAKeeper {
             ranges: Mutex::new(AreaSet::New(0, 0)),
             hugePages: Mutex::new(VecDeque::with_capacity(1000)),
             allocPages: Mutex::new(BTreeSet::new()),
+            #[cfg(target_arch = "aarch64")]
+            file_on_range: Mutex::new(HashMap::new()),
         };
     }
 
@@ -99,7 +114,8 @@ impl HostPMAKeeper {
 
     pub fn InitHugePages(&self) {
         let hugeLen = (self.ranges.lock().range.Len() / MemoryDef::ONE_GB - 2) * MemoryDef::ONE_GB;
-        //error!("InitHugePages is {:x}", self.ranges.lock().range.Len() / MemoryDef::ONE_GB - 2);
+        info!("VMM: PMA - Init with HugePages - Len is {:x}G",
+            self.ranges.lock().range.Len() / MemoryDef::ONE_GB - 2);
         let hugePageStart = self
             .RangeAllocate(hugeLen, MemoryDef::PAGE_SIZE_2M)
             .unwrap();
@@ -153,26 +169,55 @@ impl HostPMAKeeper {
         return self.Map(&mut mo, &Range::New(start, len));
     }
 
-    pub fn MapFile(&self, len: u64, prot: i32, fd: i32, offset: u64) -> Result<u64> {
-        let osfd = fd;
+    fn __map_file_directly(&self, mapping_range: Range, prot: i32, fd: i32, offset: u64)
+        -> Result<u64> {
         let mut mo = &mut MapOption::New();
-
         mo = mo
             .Proto(prot)
             .FileOffset(offset)
-            .FileId(osfd)
-            .Len(len)
-            .MapFixed();
+            .FileId(fd)
+            .Len(mapping_range.Len())
+            .Addr(mapping_range.Start())
+            .MapFixed()
+            .MapShare();
 
-        mo.MapShare();
-        // mo.MapPrecommit();
+        let ret = self.Map(&mut mo, &mapping_range)?;
+        Ok(ret)
+    }
 
-        //mo.MapLocked();
+    #[cfg(target_arch = "aarch64")]
+    fn __map_file_buffered(&self, mapping_range: Range, _prot: i32, fd: i32, offset: u64)
+        -> Result<u64> {
+        let mut file = unsafe { std::fs::File::from_raw_fd(fd) };
+        let mut rf_slice = unsafe {
+            std::slice::from_raw_parts_mut(mapping_range.Start() as *mut u8,
+                mapping_range.Len() as usize)
+        };
+        let actual_read = file.read(&mut rf_slice);
+        if actual_read.is_ok() {
+            debug!("VMM: Map file - Read from file: bytes{:#0x}", actual_read.unwrap());
+        } else {
+            debug!("VMM: Map File - read failed.");
+        }
+        debug!("VMM: Map file with FD:{}, len:{:#0x}B, host mapped:{:#0x}",
+            fd, mapping_range.Len(), mapping_range.Start());
+        self.file_on_range.lock().insert((mapping_range.Start(), mapping_range.Len()),
+            (file, offset));
+        Ok(mapping_range.Start())
+    }
 
-        let start = self.Allocate(len, MemoryDef::PMD_SIZE)?;
-        mo.Addr(start);
-        let ret = self.Map(&mut mo, &Range::New(start, len));
-        return ret;
+    pub fn MapFile(&self, length: u64, prot: i32, fd: i32, offset: u64) -> Result<u64> {
+        debug!("VMM: Map file with FD:{}, len:{:#0x} Bytes, offset:{:#0x}, PROT_flags:{:#0x}",
+            fd, length, offset, prot);
+        let start_addr = self.Allocate(length, MemoryDef::PMD_SIZE)?;
+        let mapping_range = Range {start: start_addr, len: length};
+        #[cfg(target_arch = "aarch64")]
+        if is_hw_tee() {
+            let r = self.__map_file_buffered(mapping_range, prot, fd, offset);
+            return r;
+        }
+
+        self.__map_file_directly(mapping_range, prot, fd, offset)
     }
 
     fn RangeAllocate(&self, len: u64, alignment: u64) -> Result<u64> {
@@ -224,15 +269,72 @@ impl HostPMAKeeper {
     }
 
     pub fn Unmap(&self, r: &Range) -> Result<()> {
-        //self.RemoveSeg(r);
-
         assert!(r.Start() % MemoryDef::PAGE_SIZE_2M == 0);
         assert!(r.Len() == MemoryDef::PAGE_SIZE_2M);
 
         self.FreeHugePage(r.Start());
 
+        debug!("VMM: Unmap file length:{:#0x}B, guest mapped:{:#0x}", r.Len(), r.Start());
+        #[cfg(target_arch = "aarch64")] {
+            if is_hw_tee() {
+                let f = self.file_on_range.lock().remove(&(r.Start(), r.Len()));
+                if f.is_some() {
+                    debug!("VMM: unmap file from:{:#0x} - length:{:#0x}.", r.Start(), r.Len());
+                } else {
+                    debug!("VMM: no file found at:{:#0x}", r.Start());
+                }
+            }
+        }
+
         let res = MapOption::MUnmap(r.Start(), r.Len());
         return res;
+    }
+
+    #[cfg(target_arch = "aarch64")]
+    pub fn write_back(&self, data_heap_address: u64, data_size: usize) -> u64 {
+        let ranges_array = unsafe {
+            std::slice::from_raw_parts(data_heap_address as *const Range, data_size)
+        };
+        debug!("VMM: Write back changes to the file");
+        let range_start = ranges_array[0].Start();
+        let rs_size = ranges_array[0].Len();
+        for (_key, _file) in self.file_on_range.lock().iter() {
+            if _key.0 <= range_start && (_key.0 + _key.1 > range_start) {
+                debug!("VMM: Found file buffered at:{:#0x} - length:{:#0x}", range_start, rs_size);
+                let  file_offset = _file.1;
+                let mut write_back_range: Range =
+                    Range { start: range_start, len: rs_size };
+                for i in 1..data_size {
+                    if write_back_range.Start() + write_back_range.Len() + 1
+                        == ranges_array[i].Start() {
+                        write_back_range.len += ranges_array[i].Len();
+                    } else {
+                        Self::__write_back(&_file.0, &write_back_range, file_offset, _key.0);
+                        write_back_range.start = ranges_array[i].Start();
+                        write_back_range.len = ranges_array[i].Len();
+                    }
+                }
+                Self::__write_back(&_file.0, &write_back_range, file_offset, _key.0);
+                break;
+            }
+        }
+        0
+    }
+
+    #[cfg(target_arch = "aarch64")]
+    fn __write_back(file: &std::fs::File, range: &Range, file_offset: u64, mapping_start: u64) {
+        let buff = unsafe {
+            std::slice::from_raw_parts(range.Start() as *const u8, range.Len() as usize)
+        };
+        let _offset = file_offset + (range.Start() - mapping_start);
+        let written = file.write_at(buff, _offset);
+        if written.is_ok() {
+            debug!("VMM: write back to file offset:{:#0x} - at:{:#0x}\
+                - len:{:#0x} B - written:{:#0x} B", _offset, range.Start(),
+                range.Len(), written.unwrap());
+        } else {
+            panic!("VMM: write back failed - could not write to file.");
+        }
     }
 }
 