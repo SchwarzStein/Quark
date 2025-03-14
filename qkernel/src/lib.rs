516dfdd147e7aca6fb746fc6f39836eba7219ff1
ch3lt <christob.tr@hotmail.com>
2025-03-14 15:14:30 +0100

    cc: attestation: add utility definitions needed for attestation
            * create connection with KeyBrockerService server
            * create cryptogr. keys
            * definition of required structures
            * define backend for retrieving randomness

--- qkernel/src/lib.rs
+++ qkernel/src/lib.rs
@@ -54,6 +54,19 @@ extern crate xmas_elf;
 extern crate yaxpeax_arch;
 #[cfg(feature = "snp")]
 extern crate yaxpeax_x86;
+//
+// Conf-Comp deps. - AttAg.
+//
+extern crate rsa;
+extern crate aes_gcm;
+extern crate embedded_tls;
+extern crate embedded_io;
+extern crate httparse;
+extern crate base64;
+extern crate serde;
+extern crate jwt_compact;
+extern crate kbs_types;
+extern crate getrandom;
 
 use core::panic::PanicInfo;
 use core::sync::atomic::{AtomicI32, AtomicUsize, Ordering};
